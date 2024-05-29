bbc2c5bb84b1c35a062b7476e39d70ebf580c0b7
farrasazhary <farrasazhary17@gmail.com>
2024-05-29 07:07:23 +0700

    replace database

--- latihan-oop.sql
+++ latihan-oop.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: May 26, 2024 at 07:15 PM
--- Server version: 8.0.30
--- PHP Version: 8.1.10
+-- Host: 127.0.0.1
+-- Generation Time: May 29, 2024 at 02:04 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,11 +24,36 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `buku`
+--
+
+CREATE TABLE `buku` (
+  `id_buku` int(11) NOT NULL,
+  `judul_buku` varchar(255) NOT NULL,
+  `pengarang_buku` varchar(255) NOT NULL,
+  `penerbit_buku` varchar(255) NOT NULL,
+  `tahun_terbit` year(4) NOT NULL,
+  `stok` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `buku`
+--
+
+INSERT INTO `buku` (`id_buku`, `judul_buku`, `pengarang_buku`, `penerbit_buku`, `tahun_terbit`, `stok`) VALUES
+(1, 'sdsdasdasd', 'dasdsad', 'asdasd', '2000', 11),
+(2, 'buku jadul', 'saya sendiri', 'tangerang', '2024', 200),
+(3, 'sdasd', 'sadad', 'aaa', '2024', 5000),
+(4, 'sdsadsxzcsada', 'sacasdsasdasdasd', 'dasdadasdasd', '2022', 10000);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `mahasiswa`
 --
 
 CREATE TABLE `mahasiswa` (
-  `id_mahasiswa` int NOT NULL,
+  `id_mahasiswa` int(11) NOT NULL,
   `nama_mhs` varchar(255) NOT NULL,
   `alamat_mhs` varchar(255) NOT NULL,
   `kelas_mhs` varchar(100) NOT NULL,
@@ -40,21 +65,11 @@ CREATE TABLE `mahasiswa` (
 --
 
 INSERT INTO `mahasiswa` (`id_mahasiswa`, `nama_mhs`, `alamat_mhs`, `kelas_mhs`, `no_hp`) VALUES
-(1, 'farras azhary', 'Tangerang Selatan', '07TPLP001', '082281047267');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `students`
---
-
-CREATE TABLE `students` (
-  `id` int NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `address` varchar(255) NOT NULL,
-  `class` varchar(50) NOT NULL,
-  `phone` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+(1, 'farras azhary', 'Tangerang Selatan', '07TPLP001', '082281047267'),
+(2, 'farras uye', 'farras', 'farras', '22222'),
+(3, 'ujang baru', 'tangerang', 'tplp001', '082291919'),
+(8, 'gelo amat', 'asdasd', 'asdada', '92929292'),
+(10, 'ujang betot', 'gapura sebelah', 'tplp001', '23123112233');
 
 -- --------------------------------------------------------
 
@@ -63,7 +78,7 @@ CREATE TABLE `students` (
 --
 
 CREATE TABLE `users` (
-  `id` int NOT NULL,
+  `id_user` varchar(255) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -72,12 +87,18 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`) VALUES
-(1, 'farras', '$2y$10$jrQBZ7F5o8AcbUp8xSt93eLvlYZ9MfCqX63ZNstN7HJLrh4DEHeta');
+INSERT INTO `users` (`id_user`, `username`, `password`) VALUES
+('1', 'farras', '$2y$10$jrQBZ7F5o8AcbUp8xSt93eLvlYZ9MfCqX63ZNstN7HJLrh4DEHeta');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `buku`
+--
+ALTER TABLE `buku`
+  ADD PRIMARY KEY (`id_buku`);
 
 --
 -- Indexes for table `mahasiswa`
@@ -86,16 +107,10 @@ ALTER TABLE `mahasiswa`
   ADD PRIMARY KEY (`id_mahasiswa`);
 
 --
--- Indexes for table `students`
---
-ALTER TABLE `students`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`id_user`),
   ADD UNIQUE KEY `username` (`username`);
 
 --
@@ -103,22 +118,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `buku`
+--
+ALTER TABLE `buku`
+  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `mahasiswa`
 --
 ALTER TABLE `mahasiswa`
-  MODIFY `id_mahasiswa` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `students`
---
-ALTER TABLE `students`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_mahasiswa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
